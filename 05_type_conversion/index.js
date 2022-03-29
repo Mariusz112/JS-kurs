694402f8c459817ba0f1962024f43ffcd045941f
Mariusz112 <maniek11744@gmail.com>
2022-03-29 03:11:18 +0200

    cw10

---
+++ 05_type_conversion/index.js
@@ -0,0 +1,16 @@
+/*
+let age = window.prompt("How old are you?");
+age = Number(age);
+age += 1;
+console.log("Happy Birthday! you are", age, "years old");
+*/
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("pizza");
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
---
+++ 06_const/index.js
@@ -0,0 +1,11 @@
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+//PI = 12;
+
+circumference = 2 * PI * radius;
+
+console.log("the circumference is:", circumference);
